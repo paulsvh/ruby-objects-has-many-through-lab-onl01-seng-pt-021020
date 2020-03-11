a3c885705d6a63a930379c5158183065a6d7a0ff
Paul von Hagen <pvonhagen26@gmail.com>
2020-03-11 03:38:32 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -7,6 +7,18 @@ class Doctor
     @@all << self
   end
 
+  def self.all
+    @@all
+  end
 
+  def new_appointment(name, date)
+    Appointment.new(date, name, self)
+  end
+
+  def patients
+    Patient.all.select do |patient|
+      patient.doctor == self
+    end
+  end
 
 end
